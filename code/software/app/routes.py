94f22c92171126f808f13bca34e262c27b94448f
johnli25 <warriorjohn36@gmail.com>
2022-11-07 17:39:54 -0600

    add comments

--- code/software/app/routes.py
+++ code/software/app/routes.py
@@ -4,6 +4,6 @@ from app import app
 
 @app.route("/")
 def home_page():
-    siebel4022_data = {'Task' : 'Hours per Day', 'Full' : 12, 'Not Full' : 37} # , 'Commute' : 2, 'Watching TV' : 2, 'Sleeping' : 7}
+    siebel4022_data = {'Task' : 'Hours per Day', 'Full' : 12, 'Not Full' : 37} 
 
     return render_template("pie_siebel.html",data=siebel4022_data)
