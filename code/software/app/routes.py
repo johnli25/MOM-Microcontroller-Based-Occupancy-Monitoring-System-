77630cb290f11da40d72673798aa59674848478e
johnli25 <warriorjohn36@gmail.com>
2022-11-07 16:23:03 -0600

    web app occupancy front end

--- code/software/app/routes.py
+++ code/software/app/routes.py
@@ -4,4 +4,6 @@ from app import app
 
 @app.route("/")
 def home_page():
-    return render_template("index.html")
+    siebel4022_data = {'Task' : 'Hours per Day', 'Full' : 12, 'Not Full' : 37} # , 'Commute' : 2, 'Watching TV' : 2, 'Sleeping' : 7}
+
+    return render_template("pie_siebel.html",data=siebel4022_data)
