f227163dd406caa3d0b83e458cd1ee1d1ae13142
johnli25 <warriorjohn36@gmail.com>
2022-11-08 01:51:40 -0600

    Finished web app front end FIRST TAB-still need to implement multiple tabs for multiple rooms

--- code/software/app/routes.py
+++ code/software/app/routes.py
@@ -1,9 +1,17 @@
 from flask import render_template, request, jsonify, redirect
 from flask.helpers import url_for
 from app import app
+import pytz
+from datetime import datetime
 
 @app.route("/")
 def home_page():
-    siebel4022_data = {'Task' : 'Hours per Day', 'Not Full' : 37, 'Full' : 12} 
+    not_full_count = 37
+    full_count = 12
+    d = datetime.now()
+    dt = pytz.timezone('America/Chicago').localize(d)
+    d = d.strftime('%B %d, %Y ; %I:%M:%S %p')
+    print(d)
+    siebel4022_data = {'Task' : 'Hours per Day', 'Not Full' : not_full_count, 'Full' : full_count} 
 
-    return render_template("pie_siebel.html",data=siebel4022_data)
+    return render_template("pie_siebel.html",data=siebel4022_data, full_count=full_count, now_time=d)
