a75d164959184b91cff1055d416fda38f80cd7e7
Vish GS <vishsgopal@gmail.com>
2022-11-08 19:52:52 -0600

    John pushing from Vish's lapop

--- code/software/app/connectAWS.py
+++ code/software/app/connectAWS.py
@@ -2,7 +2,19 @@ import boto3
 
 dynamo_client = boto3.client('dynamodb')
 
-# dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+# dynamodb = boto3.resource('dynamodb', region_name='us-east-1', aws_access_key)
+# AWS_ACCESS_KEY_ID = config.AWS_ACCESS_KEY_ID
+# AWS_SECRET_ACCESS_KEY = config.AWS_SECRET_ACCESS_KEY
+# REGION_NAME = config.REGION_NAME
+ 
+resource = boto3.resource(
+   'dynamodb',
+   aws_access_key_id     = "AKIA5BSW6IQX6DQ35V5T",
+   aws_secret_access_key = "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",
+   region_name           = "us-east-1"
+)
+
+MovieTable = resource.Table('Occupancy_data')
 
 def get_items():
     return dynamo_client.scan(
--- code/software/app/routes.py
+++ code/software/app/routes.py
@@ -7,18 +7,17 @@ from app import connectAWS
 
 @app.route("/")
 def home_page():
-    # not_full_count = 37
-    # full_count = 12
-    # d = datetime.now()
-    # dt = pytz.timezone('America/Chicago').localize(d)
-    # d = d.strftime('%B %d, %Y ; %I:%M:%S %p')
-    # battery = 56
-    # print(d)
-    # siebel4022_data = {'Task' : 'Hours per Day', 'Not Full' : not_full_count, 'Full' : full_count} 
-
-    # return render_template("pie_siebel.html",data=siebel4022_data, full_count=full_count, now_time=d, battery=battery)
+    not_full_count = 37
+    full_count = 12
+    d = datetime.now()
+    dt = pytz.timezone('America/Chicago').localize(d)
+    d = d.strftime('%B %d, %Y ; %I:%M:%S %p')
+    battery = 56
+    print(d)
+    siebel4022_data = {'Task' : 'Hours per Day', 'Not Full' : not_full_count, 'Full' : full_count} 
     print("test")
-    return jsonify(connectAWS.get_items())
+    return render_template("pie_siebel.html",data=siebel4022_data, full_count=full_count, now_time=d, battery=battery)
+    # return jsonify(connectAWS.get_items())
 
 # @app.route('/get-items')
 # def get_items():
