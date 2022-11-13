a753b83849a061c73d50a84012c98b203545d97a
johnli25 <warriorjohn36@gmail.com>
2022-11-12 21:23:02 -0600

    added config file, rewriting some dynamodb-flask app integration, and created template for database CRUD operations

--- code/software/app/connectAWS.py
+++ code/software/app/connectAWS.py
@@ -1,11 +1,21 @@
-# import boto3
+import boto3
+from boto3 import resource
+import config
+
+AWS_ACCESS_KEY_ID = config.AWS_ACCESS_KEY_ID
+AWS_SECRET_ACCESS_KEY = config.AWS_SECRET_ACCESS_KEY
+REGION_NAME = config.REGION_NAME
+ 
+resource = resource('dynamodb',
+   aws_access_key_id     = AWS_ACCESS_KEY_ID,
+   aws_secret_access_key = AWS_SECRET_ACCESS_KEY,
+   region_name           = REGION_NAME
+)
 
 # client = boto3.client('dynamodb',
 #   aws_access_key_id='AKIA5BSW6IQX6DQ35V5T',
 #   aws_secret_access_key='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',
 #   region_name='us-east-1')
-
-
 
 # dynamodb = boto3.resource('dynamodb')
 # table = dynamodb.Table('Occupancy_data')
