7a98d1b5b4a75248cc0b1958fd9887d042607e46
johnli25 <warriorjohn36@gmail.com>
2022-11-14 04:57:00 -0600

    added implementation for multiple tabs/rooms with a click of a button, added front end prettifications, cleaned up deprecated code and comments, and added more backend functionality. Need to fix reload bug-reloading 'moves' to the next tab smh

--- code/software/app/routes.py
+++ code/software/app/routes.py
@@ -89,31 +89,13 @@ def UpdateItemFlask(id):
         'response' : response
     }   
 
-#  Like a book
-#  Route: http://localhost:5000/like/book/<id>
-#  Method : POST
-# @app.route('/like/book/<int:id>', methods=['POST'])
-# def LikeBook(id):
-
-#     response = dynamodb_aws_handler.LikeABook(id)
-
-#     if (response['ResponseMetadata']['HTTPStatusCode'] == 200):
-#         return {
-#             'msg'      : 'Likes the book successfully',
-#             'Likes'    : response['Attributes']['likes'],
-#             'response' : response['ResponseMetadata']
-#         }
-
-#     return {
-#         'msg'      : 'Some error occured',
-#         'response' : response
-#     }
-
-# if __name__ == '__main__':
-#     app.run(host='localhost', port=5000, debug=True)
+room_idx = 1 # global static room index
+maxNumOfRoomsInDB = 2 # global static max # of rooms in DB
 
 @app.route("/")
 def home_page():
+    # global room_idx
+    # room_idx = (room_idx + 1) % 2
     # dynamodb_aws_handler.create_occupancy_table()
     print(dynamodb_aws_handler.Occupancy_table)
     response = dynamodb_aws_handler.Occupancy_table.get_item(Key={
@@ -121,34 +103,55 @@ def home_page():
         "device_id": 22
     })
     # data = response['Items']
-
     # while 'LastEvaluatedKey' in response:
     #     response = dynamodb_aws_handler.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
     #     data.extend(response['Items'])
-    print("--------a run--------")
     items = dynamodb_aws_handler.Occupancy_table.scan()['Items']
-    print(type(items))
-    for item in items:
-        print (type(item))
-        print(item.keys())
-        print(item['device_data']['battery'])
-        print(item['device_data']['counts'])
-        print(item['device_data']['location'])
+    # print(type(items))
+    # for item in items:
+    #     print (type(item))
+    #     print(item.keys())
+    #     print(item['device_data']['battery'])
+    #     print(item['device_data']['counts'])
+    #     print(item['device_data']['location'])
 
     not_full_count = 37
-    full_count = int(items[0]['device_data']['counts'])
+    full_count = int(items[room_idx]['device_data']['counts'])
     d = datetime.now()
     dt = pytz.timezone('America/Chicago').localize(d)
     d = d.strftime('%B %d, %Y ; %I:%M:%S %p')
-    battery = items[0]['device_data']['battery']
+    battery = items[room_idx]['device_data']['battery']
     print(battery)
     print(d)
-    location = items[0]['device_data']['location']
+    location = items[room_idx]['device_data']['location']
     siebel4022_data = {'Task' : 'Hours per Day', 'Not Full' : not_full_count, 'Full' : full_count} 
-    device_id = items[0]['device_id']
+    device_id = items[room_idx]['device_id']
     return render_template("pie_siebel.html",data=siebel4022_data, full_count=full_count, now_time=d, battery=battery, device_id=device_id,
     location = location)
     # return jsonify(connectAWS.get_items())
+
+@app.route("/update")
+def update_home_page():
+    global room_idx
+    room_idx = (room_idx + 1) % 2
+    response = dynamodb_aws_handler.Occupancy_table.get_item(Key={
+        "sample_time": 1667948705117,
+        "device_id": 22
+    })
+    items = dynamodb_aws_handler.Occupancy_table.scan()['Items']
+    not_full_count = 37
+    full_count = int(items[room_idx]['device_data']['counts'])
+    d = datetime.now()
+    dt = pytz.timezone('America/Chicago').localize(d)
+    d = d.strftime('%B %d, %Y ; %I:%M:%S %p')
+    battery = items[room_idx]['device_data']['battery']
+    print(battery)
+    print(d)
+    location = items[room_idx]['device_data']['location']
+    siebel4022_data = {'Task' : 'Hours per Day', 'Not Full' : not_full_count, 'Full' : full_count} 
+    device_id = items[room_idx]['device_id']
+    return render_template("pie_siebel.html",data=siebel4022_data, full_count=full_count, now_time=d, battery=battery, device_id=device_id,
+    location = location)
 
 class DB:
     def retrieve_value(sensor, metric=None):
